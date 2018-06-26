71e6e740de605f97a7771c8abf79ffc05e103117
m-toman <m.toman@neuratec.com>
2018-06-26 10:21:36 +0200

    Closure for hyperparameters

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # ecg-loss
 Tensorflow implementation for ε-contaminated Gausssian distribution loss with Tensorflow, Keras-compatible.
 
-# References
+## Usage
+
+
+## References
 J. Tukey, “A survey of sampling from contaminated distributions,”
 Contributions to probability and statistics, vol. 2, pp. 448–485,
 1960.
--- ecg_loss.py
+++ ecg_loss.py
@@ -1,13 +1,33 @@
+""" Implementation of e-contaminated Gaussian distribution loss
+ to be used with Tensorflow or Keras with Tensorflow backend.
+
+ J. Tukey, “A survey of sampling from contaminated distributions,”
+ Contributions to probability and statistics, vol. 2, pp. 448–485, 1960.
+"""
+
 import tensorflow as tf
 import tensorflow.contrib.distributions as tfd
 import numpy as np
 from scipy.stats import multivariate_normal
 
-ecg_c = 10.0
-ecg_epsilon = 0.1
+
+def get_ecg_loss(ecg_c=10.0, ecg_epsilon=0.1):
+    """ Returns a function with two parameters y_true and y_pred with parameters
+        ecg_c and ecg_epsilon fixed (captured by closure).
+        This allows usage with Keras as: model.compile(loss=get_ecg_loss(5.0, 0.2)).
+    """
+    def ecg_loss(y_true, y_pred):
+        num_dims = y_pred.get_shape().as_list()[1]
+        n = tfd.MultivariateNormalDiag(
+            loc=y_pred, scale_diag=tf.ones(num_dims)).prob(y_true)
+        nc = tfd.MultivariateNormalDiag(
+            loc=y_pred, scale_diag=tf.ones(num_dims) * ecg_c).prob(y_true)
+        return tf.reduce_mean(tf.log((1.0 - ecg_epsilon) * n + ecg_epsilon * nc) * -1.0)
+    return ecg_loss
 
 
-def ecg_loss_np(y_true, y_pred):
+def ecg_loss_np(y_true, y_pred, ecg_c=10.0, ecg_epsilon=0.1):
+    """ Naive numpy reference implementation. """
     assert (y_true.shape == y_pred.shape)
     losses = []
     for row in range(y_true.shape[0]):
@@ -22,15 +42,6 @@ def ecg_loss_np(y_true, y_pred):
     return np.mean(losses)
 
 
-def ecg_loss(y_true, y_pred):
-    num_dims = y_pred.get_shape().as_list()[1]
-    n = tfd.MultivariateNormalDiag(
-        loc=y_pred, scale_diag=tf.ones(num_dims)).prob(y_true)
-    nc = tfd.MultivariateNormalDiag(
-        loc=y_pred, scale_diag=tf.ones(num_dims) * ecg_c).prob(y_true)
-    return tf.reduce_mean(tf.log((1.0 - ecg_epsilon) * n + ecg_epsilon * nc) * -1.0)
-
-
 if __name__ == "__main__":
     with tf.Session() as sess:
         y_true = np.random.rand(5, 10)
@@ -40,7 +51,7 @@ if __name__ == "__main__":
 
         y_true_tf = tf.placeholder(tf.float32, y_true.shape)
         y_pred_tf = tf.placeholder(tf.float32, y_pred.shape)
-        loss_tf = ecg_loss(y_true_tf, y_pred_tf)
+        loss_tf = get_ecg_loss()(y_true_tf, y_pred_tf)
 
         loss_tf_result = sess.run(loss_tf,
                                   feed_dict={y_true_tf: y_true,
